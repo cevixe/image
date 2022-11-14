58da52afb7b538c1309f4cb0223e806b2bb9ebe4
Ronnie Ayala <ronnie.ayala@outlook.com>
2022-11-14 01:52:53 -0600

    add permission to lambda for cloudwatch

--- cdk/service/lambda/newfnrole.go
+++ cdk/service/lambda/newfnrole.go
@@ -2,10 +2,24 @@ package lambda
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	"github.com/aws/jsii-runtime-go"
 	"github.com/cevixe/cdk/module"
 	"github.com/cevixe/cdk/service/iam"
 )
 
 func NewFunctionRole(mod module.Module, alias string) awsiam.Role {
-	return iam.NewServiceRole(mod, alias, "lambda.amazonaws.com")
+	role := iam.NewServiceRole(mod, alias, "lambda.amazonaws.com")
+	role.AddToPrincipalPolicy(awsiam.NewPolicyStatement(
+		&awsiam.PolicyStatementProps{
+			Effect: awsiam.Effect_ALLOW,
+			Actions: &[]*string{
+				jsii.String("logs:CreateLogGroup"),
+				jsii.String("logs:CreateLogStream"),
+				jsii.String("logs:PutLogEvents"),
+			},
+			Resources: &[]*string{
+				jsii.String("*"),
+			},
+		}))
+	return role
 }
