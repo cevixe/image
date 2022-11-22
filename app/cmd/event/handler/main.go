e748b451ab9d88bce0357038b310b07e45709881
Ronnie Ayala <ronnie.ayala@outlook.com>
2022-11-21 22:55:38 -0600

    fix event handler

--- app/cmd/event/handler/main.go
+++ app/cmd/event/handler/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/jsii-runtime-go"
 	"github.com/cevixe/sdk/client/config"
 	"github.com/cevixe/sdk/message"
 	"github.com/pkg/errors"
@@ -19,17 +21,47 @@ type Handler struct {
 
 func (h *Handler) Handle(ctx context.Context, request events.SQSEvent) error {
 
-	messages := make([]message.Message, 0)
+	writeItems := make([]types.TransactWriteItem, 0)
 	for _, record := range request.Records {
 
 		item, err := message.FromSQS(record)
 		if err != nil {
 			return errors.Wrap(err, "cannot read message from sqs")
 		}
-		messages = append(messages, item)
+
+		itemMap, err := message.ToDynamodb_Map(item)
+		if err != nil {
+			return errors.Wrap(err, "cannot marshal message to dynamodb map")
+		}
+
+		for key, value := range itemMap {
+			if value == nil {
+				delete(itemMap, key)
+			} else {
+				switch value.(type) {
+				case *types.AttributeValueMemberNULL:
+					delete(itemMap, key)
+				default:
+					continue
+				}
+			}
+		}
+
+		writeItems = append(writeItems, types.TransactWriteItem{
+			Put: &types.Put{
+				TableName:           jsii.String(h.table),
+				Item:                itemMap,
+				ConditionExpression: jsii.String("attribute_not_exists(#id)"),
+				ExpressionAttributeNames: map[string]string{
+					"#id": "id",
+				},
+			},
+		})
 	}
 
-	err := message.Write(ctx, messages...)
+	_, err := h.client.TransactWriteItems(ctx, &dynamodb.TransactWriteItemsInput{
+		TransactItems: writeItems,
+	})
 	if err != nil {
 		return errors.Wrap(err, "cannot read message from sqs")
 	}
