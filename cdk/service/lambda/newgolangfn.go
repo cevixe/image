2f9b03b3c605e92490599c2843bdc333521a6522
rafaelfernandezve <116745450+rafaelfernandezve@users.noreply.github.com>
2024-04-11 08:35:53 -0500

    Update newgolangfn.go

--- cdk/service/lambda/newgolangfn.go
+++ cdk/service/lambda/newgolangfn.go
@@ -28,7 +28,7 @@ func NewGolangFunction(mod module.Module, alias string, entry string) awslambda.
 		Timeout:      awscdk.Duration_Seconds(jsii.Number(10)),
 		Entry:        jsii.String(entryPath),
 		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
-		LogRetention: awslogs.RetentionDays_ONE_MONTH,
+		//LogRetention: awslogs.RetentionDays_ONE_MONTH,
 		Role:         role,
 		Bundling: &awsgo.BundlingOptions{
 			CgoEnabled: jsii.Bool(false),
