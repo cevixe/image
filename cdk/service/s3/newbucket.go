fa4b2c6efbc90b8392d8e40303958fbe4b1d3f18
rafaelfernandezve <116745450+rafaelfernandezve@users.noreply.github.com>
2024-04-11 08:56:21 -0500

    Update newbucket.go

--- cdk/service/s3/newbucket.go
+++ cdk/service/s3/newbucket.go
@@ -1,7 +1,7 @@
 package s3
 
 import (
-	"github.com/aws/aws-cdk-go/awscdk/v2"
+	//"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cevixe/cdk/module"
